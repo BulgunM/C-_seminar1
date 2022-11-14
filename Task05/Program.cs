2d7373a1019d5f24e4529d4fc4baeffcf14ca819
Bulgun <bulgunya@mail.ru>
2022-11-15 00:09:16 +0300

    task0

---
+++ Task05/Program.cs
@@ -0,0 +1,13 @@
+﻿// на входе натуральное (которое считает) целое число
+// на выходе все числа в промежутке от -N до N
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = -number;
+
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count++;
+}
+
