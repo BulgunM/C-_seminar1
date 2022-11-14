f2e610a423c1ecca6636f8d69010fcbbcd0b236d
Bulgun <bulgunya@mail.ru>
2022-11-15 00:07:55 +0300

    01task

---
+++ Task01/Program.cs
@@ -0,0 +1,23 @@
+﻿// на вход принимает 2 числа
+// проверяет: первое квадрат второго или нет
+// 25, 5 - да
+// 2, 10 - нет
+// 9, -3 - да
+// -3, 9 - нет
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int square = number2 * number2;
+
+if (square == number1)
+{
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number1} не является квадратом числа {number2}");
+}
