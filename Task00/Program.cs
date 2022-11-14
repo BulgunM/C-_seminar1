11ce8bfddaf12b224ba649e20ac7de895e74e7ce
Bulgun <bulgunya@mail.ru>
2022-11-15 00:05:23 +0300

    task01

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает на вход число
+// выдает квадрат числа
+
+Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine()); // Convert.ToInt32 - преобразовывает строку, которую ввели через ReadLine, в переменную int
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
