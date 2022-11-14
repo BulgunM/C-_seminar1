54789a4ec4b74ee7cc3c803cd2d361eeec797cb1
Bulgun <bulgunya@mail.ru>
2022-11-15 00:09:50 +0300

    task07

---
+++ Task07/Program.cs
@@ -0,0 +1,7 @@
+﻿// на вход трехзначное число
+// на выход отображает последнюю цифру числа
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 10;
+Console.WriteLine($"Последняя цифра {result}");
